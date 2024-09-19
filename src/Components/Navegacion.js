cda96dd14feb10f74c7efd250711952b9d844c3c
AlejandroGutierrez2 <alejandrojosue70@gmail.com>
2024-09-19 16:24:43 -0500

    primer commit

---
+++ src/Components/Navegacion.js
@@ -0,0 +1,13 @@
+function Navegacion() {
+    <>
+        <nav>
+            <ul className="listNav">
+                <li>Contacto</li>
+            <li className="navbar-item"><Link to="/">Inicio</Link></li>
+            <li className="navbar-item"><Link to="/about">Acerca de</Link></li>
+            <li className="navbar-item"><Link to="/contact">Contacto</Link></li>
+            </ul>
+        </nav>
+    </>
+};
+export default Navegacion;
